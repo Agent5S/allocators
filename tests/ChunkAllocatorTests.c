6c8cb30cb4ca1c312a70092354a350a480bab2ef
Agent5 <19892761+Agent5S@users.noreply.github.com>
2023-02-12 15:42:22 -0600

    Test assert cases

--- tests/ChunkAllocatorTests.c
+++ tests/ChunkAllocatorTests.c
@@ -9,6 +9,7 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
+#include <stdio.h>
 #include <cmocka.h>
 
 typedef struct 
@@ -18,11 +19,11 @@ typedef struct
 } TestStruct;
 
 static void test_chunk_alloc_init(void** state) {
-    ChunkAlloc alloc;
-    //TODO: Test asserts
-    //expect_assert_failure(ChunkAlloc_Init(&alloc, sizeof(size_t) - 1, 16));
-    
     const int arrayLen = 4;
+
+    ChunkAlloc alloc;
+    //Test initialization with smaller-than-allowed size
+    expect_assert_failure(ChunkAlloc_Init(&alloc, sizeof(size_t) - 1, 16));
     //Test normal initialization
     assert_return_code(ChunkAlloc_Init(&alloc, sizeof(TestStruct), arrayLen), 0);
     assert_non_null(alloc.pool);
@@ -33,7 +34,7 @@ static void test_chunk_alloc_init(void** state) {
         TestStruct* chunk = &((TestStruct*)alloc.pool)[i];
         assert_ptr_equal(*(TestStruct**)chunk, &(((TestStruct*)alloc.pool)[i + 1]));
     }
-    assert_null(((TestStruct**)alloc.pool)[3]);
+    assert_null(((TestStruct**)alloc.pool)[arrayLen - 1]);
 
     //TODO: Test malloc failure
 
@@ -41,9 +42,10 @@ static void test_chunk_alloc_init(void** state) {
 }
 
 static void test_chunk_alloc_deinit(void** state) {
-    ChunkAlloc alloc;
+    const int arrayLen = 4;
 
-    ChunkAlloc_Init(&alloc, sizeof(TestStruct), 16);
+    ChunkAlloc alloc;
+    ChunkAlloc_Init(&alloc, sizeof(TestStruct), arrayLen);
     ChunkAlloc_Deinit(&alloc);
     assert_null(alloc.pool);
     assert_null(alloc.current);
@@ -52,41 +54,49 @@ static void test_chunk_alloc_deinit(void** state) {
 }
 
 static void test_chunk_alloc_get(void** state) {
+    const int arrayLen = 4;
+
     ChunkAlloc alloc;
-    ChunkAlloc_Init(&alloc, sizeof(TestStruct), 8);
+    ChunkAlloc_Init(&alloc, sizeof(TestStruct), arrayLen);
 
     TestStruct* chunk = ChunkAlloc_Get(&alloc);
     assert_non_null(chunk);
     assert_ptr_equal(alloc.current, (TestStruct*)alloc.pool + 1);
     ChunkAlloc_Free(&alloc, chunk);
-    //Correct usages should assign NULL to the chunk pointer
+    chunk = NULL;
 
     ChunkAlloc_Deinit(&alloc);
 }
 
 static void test_chunk_alloc_get_multiple(void** state) {
-    ChunkAlloc alloc;
-    ChunkAlloc_Init(&alloc, sizeof(TestStruct), 4);
+    const int arrayLen = 4;
 
-    TestStruct* chunks[4];
-    for (int i = 0; i < 4; i++) {
+    ChunkAlloc alloc;
+    ChunkAlloc_Init(&alloc, sizeof(TestStruct), arrayLen);
+
+    TestStruct* chunks[arrayLen];
+    for (int i = 0; i < arrayLen; i++) {
         chunks[i] = ChunkAlloc_Get(&alloc);
         assert_non_null(chunks[i]);
     }
     assert_null(alloc.current);
-    for (int i = 0; i < 4; i++) {
+
+    //Test overallocating
+    expect_assert_failure(ChunkAlloc_Get(&alloc));
+
+    for (int i = 0; i < arrayLen; i++) {
         ChunkAlloc_Free(&alloc, chunks[i]);
-        //Correct usages should assign NULL to the chunk pointer
+        chunks[i] = NULL;
     }
-
-    //TODO: Test overflow
-
+    
     ChunkAlloc_Deinit(&alloc);
 }
 
 static void test_chunk_alloc_free(void** state) {
+    const int arrayLen = 4;
+
     ChunkAlloc alloc;
-    ChunkAlloc_Init(&alloc, sizeof(TestStruct), 8);
+    ChunkAlloc_Init(&alloc, sizeof(TestStruct), 4);
 
     TestStruct* chunk = ChunkAlloc_Get(&alloc);
     assert_ptr_equal(alloc.current, (TestStruct*)alloc.pool + 1);
@@ -96,63 +106,74 @@ static void test_chunk_alloc_free(void** state) {
     assert_ptr_equal(*(void**)alloc.current, chunk + 1);
     //Correct usages should assign NULL to the chunk pointer
 
+    //Test freeing memory out of the pool
+    expect_assert_failure(ChunkAlloc_Free(&alloc, chunk - 1));
+    expect_assert_failure(ChunkAlloc_Free(&alloc, chunk + arrayLen));
+
     //Test double freeing
+    expect_assert_failure(ChunkAlloc_Free(&alloc, chunk));
 
     ChunkAlloc_Deinit(&alloc);
 }
 
 static void test_chunk_alloc_free_multiple(void** state) {
-    ChunkAlloc alloc;
-    ChunkAlloc_Init(&alloc, sizeof(TestStruct), 4);
+    const int arraySize = 4;
 
-    TestStruct* chunks[4];
-    for (int i = 0; i < 4; i++) {
+    ChunkAlloc alloc;
+    ChunkAlloc_Init(&alloc, sizeof(TestStruct), arraySize);
+
+    TestStruct* chunks[arraySize];
+    for (int i = 0; i < arraySize; i++) {
         chunks[i] = ChunkAlloc_Get(&alloc);
         assert_non_null(chunks[i]);
     }
     assert_null(alloc.current);
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < arraySize; i++) {
         ChunkAlloc_Free(&alloc, chunks[i]);
         assert_ptr_equal(alloc.current, chunks[i]);
-        //Correct usages should assign NULL to the chunk pointer
+        chunks[i] = NULL;
     }
 
     ChunkAlloc_Deinit(&alloc);
 }
 
 static void test_chunk_alloc_get_free_multiple(void** state) {
-    ChunkAlloc alloc;
-    ChunkAlloc_Init(&alloc, sizeof(TestStruct), 4);
+    uint8_t deallocArray[4] = { 2, 3, 1, 0 };
+    const int arraySize = sizeof(deallocArray);
+    const int deallocIdx1 = 0;
+    const int deallocIdx2 = 2;
 
-    TestStruct* chunks[4];
-    for (int i = 0; i < 4; i++) {
+    ChunkAlloc alloc;
+    ChunkAlloc_Init(&alloc, sizeof(TestStruct), arraySize);
+
+    TestStruct* chunks[arraySize];
+    for (int i = 0; i < arraySize; i++) {
         chunks[i] = ChunkAlloc_Get(&alloc);
         assert_non_null(chunks[i]);
     }
     assert_null(alloc.current);
 
-    ChunkAlloc_Free(&alloc, chunks[0]);
-    assert_ptr_equal(alloc.current, chunks[0]);
+    ChunkAlloc_Free(&alloc, chunks[deallocIdx1]);
+    assert_ptr_equal(alloc.current, chunks[deallocIdx1]);
     assert_null(*(TestStruct**)alloc.current);
     //Correct usages should assign NULL to the chunk pointer
 
-    ChunkAlloc_Free(&alloc, chunks[2]);
-    assert_ptr_equal(alloc.current, chunks[2]);
-    assert_ptr_equal(*(TestStruct**)alloc.current, chunks[0]);
+    ChunkAlloc_Free(&alloc, chunks[deallocIdx2]);
+    assert_ptr_equal(alloc.current, chunks[deallocIdx2]);
+    assert_ptr_equal(*(TestStruct**)alloc.current, chunks[deallocIdx1]);
     //Correct usages should assign NULL to the chunk pointer
 
-    chunks[0] = ChunkAlloc_Get(&alloc);
+    chunks[deallocIdx1] = ChunkAlloc_Get(&alloc);
     assert_ptr_equal(alloc.current, alloc.pool);
 
-    chunks[2] = ChunkAlloc_Get(&alloc);
+    chunks[deallocIdx2] = ChunkAlloc_Get(&alloc);
     assert_null(alloc.current);
 
-    uint8_t deallocOrder[4] = { 2, 3, 1, 0 };
-    for (int i = 0; i < 4; i++) {
-        uint8_t idx = deallocOrder[i];
+    for (int i = 0; i < arraySize; i++) {
+        uint8_t idx = deallocArray[i];
         ChunkAlloc_Free(&alloc, chunks[idx]);
         assert_ptr_equal(alloc.current, chunks[idx]);
-        //Correct usages should assign NULL to the chunk pointer
+        chunks[idx] = NULL;
     }
 
     ChunkAlloc_Deinit(&alloc);
